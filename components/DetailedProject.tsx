1c2129cd2489f83d91f159efbd14120eda1f20f7
owni14 <kmwoo514@gmail.com>
2023-01-12 21:53:54 +0900

    in progress: project 상세페이지 footer 제일 밑으로 내려가게 구현 진행 중, 레이아웃 70%정도 진행 완료

--- components/DetailedProject.tsx
+++ components/DetailedProject.tsx
@@ -1,14 +1,39 @@
 const DetailedProject = () => {
   return (
     <div className='animate-fade-in-up'>
-      <div className='mb-36 mt-20 text-7xl'>
+      <div className='mb-12 mt-8 text-5xl'>
         <p className='mb-4'>Project Name</p>
       </div>
-      <div>Deployment Link</div>
-      <div>Summary and main function</div>
-      <div>Background</div>
-      <div>Realization</div>
-      <div>Technology Stacks</div>
+      <hr className='mb-8' />
+      <div>
+        <div className='text-4xl mb-4'>📌 Summary</div>
+        <p className='mb-8'>test summay</p>
+      </div>
+      <div>
+        <div className='text-4xl mb-4'>💡 Meaning</div>
+        <p className='mb-8'>test meaning</p>
+      </div>
+      <div className='flex flex-row items-start w-full justify-between'>
+        <div className='w-6/12'>
+          <div className=' text-4xl mb-4'>🔗 Deployment Link</div>
+          <p>test link</p>
+        </div>
+        <div className='w-6/12'>
+          <div className=' text-4xl mb-4'>🛠️ Technology Stacks</div>
+          <div className='flex flex-row'>
+            <p className='mr-2'>Front End :</p>
+            <p>test</p>
+          </div>
+          <div className='flex flex-row'>
+            <p className='mr-2'>Back End :</p>
+            <p>test</p>
+          </div>
+          <div className='flex flex-row'>
+            <p className='mr-2'>Deployment :</p>
+            <p>test</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
