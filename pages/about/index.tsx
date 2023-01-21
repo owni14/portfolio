622859d19af2b989666b996627110c66ec24ed68
owni14 <kmwoo514@gmail.com>
2023-01-21 16:35:02 +0900

    [style] about layout 수정

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -12,12 +12,19 @@ const about = () => {
     <div className='animate-fade-in-up'>
       <h1 className='text-5xl font-bold'>About Me</h1>
       <div className='border-2 w-40 ml-[142px] mb-12'></div>
-      <div className='flex flex-row justify-between items-start mb-10 font-light'>
+      <div className='flex flex-row justify-between items-center font-light'>
         <Image src={profile} width={600} alt='Picture of me' />
-        <div className='w-6/12 flex flex-col justify-between'>
+        <div className='w-6/12 flex flex-col justify-evenly'>
+          <div className='text-2xl mb-8'>
+            <p>안녕하세요🙇‍♂️</p>
+            <p>
+              꾸준하고 성실하게 공부하며 어제보다 더 나은 오늘을 만들기 위해
+              노력하는 프론트엔드 개발자 김민우입니다.
+            </p>
+          </div>
           {aboutContent.map((item) => (
-            <div className='flex flex-row justify-between items-center text-4xl h-[60px]'>
-              <div>{item.title}</div>
+            <div className='flex flex-row text-2xl h-[40px]'>
+              <div className='w-[120px]'>{item.title}</div>
               {item.content === `Min's Blog` ? (
                 <Link
                   className='hover:text-gray-500 hover:duration-300'
@@ -37,10 +44,6 @@ const about = () => {
               )}
             </div>
           ))}
-          <div className='text-3xl mt-4'>
-            안녕하세요🙇‍♂️ 꾸준하고 성실하게 공부하며 어제보다 더 나은 오늘을
-            만들기 위해 노력하는 프론트엔드 개발자 김민우입니다.
-          </div>
         </div>
       </div>
       <div className='font-regular'></div>
