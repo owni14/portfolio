ced57349d90c7db02890107f6fe2c8a641384591
owni14 <kmwoo514@gmail.com>
2023-01-29 16:06:50 +0900

    style: carpool service svg 크기에 맞춰 보여지게 구현

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import Link from 'next/link';
 import { ProjectListProps } from '../types';
+import CarpoolServiceImage from '../assets/images/carpoolService.svg';
 
 const Card = (props: ProjectListProps) => {
   return (
     <div className='max-h-[35rem]'>
       <Link className='group' href={`/project/${props.id}`}>
         <div className='max-h-[450px] overflow-hidden '>
-          <img
-            className='group-hover:scale-105 group-hover:duration-500'
-            src={props.image}
-            alt='project_img'
-          />
+          {props.id === 'carpool_service' && (
+            <CarpoolServiceImage className='group-hover:scale-105 group-hover:duration-500' />
+          )}
         </div>
         <div>
           <h1 className='my-4 text-2xl font-bold group-hover:text-gray-400 group-hover:duration-500 font-bold'>
