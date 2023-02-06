579ae0452c78b3cce6e9da1d6df6c5c3637138e4
owni14 <kmwoo514@gmail.com>
2023-02-06 22:51:05 +0900

    :lipstick: style: about UI 반응형 제작

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -10,18 +10,18 @@ const content =
 const about = () => {
   return (
     <div className='animate-fade-in-up'>
-      <h1 className='text-5xl font-appleSemiBold laptopM:text-center laptopM:underline laptopM:mb-12 tablet:text-4xl'>
+      <h1 className='text-5xl font-appleSemiBold laptopM:text-center laptopM:underline laptopM:mb-12 tablet:text-4xl mobileL:text-3xl mobileS:text-2xl'>
         About Me
       </h1>
       <div className='border-2 w-40 ml-[142px] mb-12 laptopM:hidden'></div>
       <div className='flex flex-row justify-between items-center font-appleRegular laptopM:flex-col laptopM:gap-10'>
         <Image
-          className='w-[600px] tablet:w-[400px]'
+          className='w-[600px] tablet:w-[400px] mobileL:w-[300px] mobileS:w-[200px]'
           src={profile}
           alt='Picture of me'
         />
-        <div className='w-6/12 laptopM:w-[600px] tablet:w-[400px]'>
-          <div className='text-2xl mb-8 tablet:text-xl'>
+        <div className='w-6/12 laptopM:w-[600px] tablet:w-[400px] mobileL:w-[300px] mobileS:w-[200px]'>
+          <div className='text-2xl mb-8 tablet:text-xl mobileL:text-base mobileS:text-sm'>
             <p>안녕하세요🙇‍♂️</p>
             <p>
               꾸준하고 성실하게 공부하며 어제보다 더 나은 오늘을 만들기 위해
@@ -30,10 +30,12 @@ const about = () => {
           </div>
           {aboutContent.map((item) => (
             <div
-              className='flex flex-row text-2xl h-[40px] tablet:text-xl'
+              className='flex flex-row text-2xl h-[40px] tablet:text-xl mobileL:text-base mobileS:text-xs'
               key={item.title}
             >
-              <div className='w-[130px]'>{item.title}</div>
+              <div className='w-[130px] mobileL:w-[100px] mobileS:w-[65px]'>
+                {item.title}
+              </div>
               {item.content === `Min's Blog` ? (
                 <Link
                   className='hover:text-gray-500 hover:duration-300'
