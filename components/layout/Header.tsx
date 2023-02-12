ebe97ac5f971a4c1610c89914d3b9dfcf3d75aa8
owni14 <kmwoo514@gmail.com>
2023-02-12 12:51:56 +0900

    :art: style: header 반응형 제작

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,15 +1,15 @@
-import Link from 'next/link';
-import { useContext } from 'react';
-import ClickState from '../../store/clickState';
+import Link from "next/link";
+import { useContext } from "react";
+import ClickState from "../../store/clickState";
 
 const HeaderBar = () => {
-  const navLink = ['Projects', 'About', 'Skills', 'Career'];
+  const navLink = ["Projects", "About", "Skills", "Career"];
 
   const [isClick, setIsClick] = useContext<any>(ClickState);
 
   return (
-    <div className='flex flex-row flex-wrap content-center justify-between h-[120px] text-xl sticky top-0 bg-neutral-900 z-10 font-regular'>
-      <div>
+    <div className='flex flex-row flex-wrap content-center justify-between h-[120px] text-xl sticky top-0 bg-neutral-900 z-10 font-regular tablet:text-lg mobileL:flex-col mobileL:items-center mobileL:h-[90px] mobileL:pb-[15px] mobileL:pt-[15px]'>
+      <div className='text-xl'>
         <Link
           className='hover:opacity-50 hover:duration-500 font-appleSemiBold'
           href='/'
@@ -19,10 +19,10 @@ const HeaderBar = () => {
         </Link>
       </div>
       <nav>
-        <ul className='flex flex-row gap-10 list-none font-appleSemiBold'>
+        <ul className='flex flex-row gap-10 list-none font-appleSemiBold tablet:gap-3'>
           {navLink.map((link) => (
             <li className='float-left' key={link}>
-              {link === 'Projects' ? (
+              {link === "Projects" ? (
                 <Link
                   href='/'
                   as={`/#project-list`}
